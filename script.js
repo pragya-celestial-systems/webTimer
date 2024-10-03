cc845c7ab8e337115b68ab645fbf4b4c712f2719
Celestial <celestial@CELSYSNCROOO036.local>
2024-10-03 10:16:58 +0530

    fixed bugs, added validations

--- script.js
+++ script.js
@@ -1,66 +1,134 @@
-const minutesEl = document.querySelector('#minutes');
-const secondsEl = document.querySelector('#seconds');
-const startBtn = document.querySelector('#startBtn');
-const timerEl = document.querySelector('#timer');
-const alarm = document.querySelector('#alarm')
+const inputContainer = document.querySelector("#topContainer");
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const startBtn = document.querySelector("#startBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const timerEl = document.querySelector("#timer");
+const alarm = document.querySelector("#alarm");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const buttons = [btn1, btn2, btn3];
 
-let minutes, seconds, totalTime;
+let minutes = 0,
+  seconds = 0,
+  totalTime = 0;
+let timer;
 
-minutesEl.addEventListener('input', (e) => {
-    minutes = e.target.value * 60;
+localStorage.setItem("time", totalTime);
 
-    if (minutes) {
-        if (seconds) {
-            totalTime = minutes + seconds;
-        } else {
-            totalTime = minutes;
-        }
-    }
+function startTimer() {
+  if (timer) {
+    clearInterval(timer);
+  }
 
-    localStorage.setItem("time", totalTime);
-})
+  let time = localStorage.getItem("time");
+  if (time > 0) {
+    minutesEl.disabled = true;
+    secondsEl.disabled = true;
+    startBtn.disabled = true;
 
-secondsEl.addEventListener('input', (e) => {
-    seconds = +e.target.value;
-    if (seconds) {
-      if (minutes) {
-        totalTime = minutes + seconds;
-      } else {
-        totalTime = seconds;
+    timer = setInterval(() => {
+      time--;
+      localStorage.setItem("time", time);
+      timerEl.textContent = `Time Left: ${time}`;
+
+      if (time <= 0) {
+        clearInterval(timer);
+        timerEl.textContent = `Time up!!!`;
+        alarm.play();
+        resetInputs();
       }
-    }
-
-    localStorage.setItem("time", totalTime);
-})
-
-// start the timer as soon as user clicks the start button
-
-startBtn.addEventListener('click', () => {
-    if (totalTime <= 0 || !totalTime) return;
-    // disable the start btn
-    startBtn.disable = true;
-    // let time = localStorage.getItem('time');
-
-    // if (time > 0) {
-    
-    //     let timer = setInterval(() => {
-    //         localStorage.setItem('time', time - 1);
-    //         console.log('time left  : ' ,time);
-    //         if (time <= 0) {
-    //             clearInterval(timer);
-    //             console.log("Timer Ended!");
-    //         };
-    //     }, time)
-    
-    // }
-    let timer = setInterval(() => {
-        timerEl.textContent = `Time left : ${totalTime}`
-
-        if (totalTime <= 0) {
-            clearInterval(timer)
-            timerEl.textContent = `Time up!!!`;
-            alarm.play()
-        }
-
     }, 1000);
+  }
+}
+
+
+function resetInputs() {
+  minutesEl.disabled = false;
+  secondsEl.disabled = false;
+  startBtn.disabled = false;
+//   resetBtn.disabled = true;  
+  minutesEl.value = "";
+  secondsEl.value = "";
+}
+
+function setTotalTime() {
+  totalTime = (minutes * 60 || 0) + (seconds || 0);
+  localStorage.setItem("time", totalTime);
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const [minStr, secStr] = btn.textContent.split(":");
+    minutes = parseInt(minStr.trim());
+    seconds = parseInt(secStr.trim());
+
+      if (seconds < 0 || seconds > 60) {
+        alert("Seconds are invalid.");
+        return;
+      }
+
+      if (minutes < 0) {
+        alert("Minutes are invalid.");
+        return;
+      }
+
+    setTotalTime();
+    resetInputs();
+    startTimer();
+  });
+});
+
+minutesEl.addEventListener("input", (e) => {
+
+  if (e.target.value < 0) {
+    alert("Minutes are invalid.");
+    e.target.value = '';
+    localStorage.setItem("time", 0);
+    return;
+  }
+
+  minutes = parseInt(e.target.value) || 0;
+
+  setTotalTime();
+});
+
+secondsEl.addEventListener("input", (e) => {
+  if (e.target.value < 0 || e.target.value > 60) {
+    alert('Seconds are invalid.');
+    e.target.value = '';
+    localStorage.setItem('time', 0);
+    return;
+  }
+
+  
+  seconds = parseInt(e.target.value) || 0;
+  setTotalTime();
+});
+
+
+startBtn.addEventListener("click", () => {
+  if (totalTime > 0) {
+    this.disabled = true;
+    startTimer();
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+    if (timer) {
+        clearInterval(timer);
+    }
+      resetInputs()
+      timerEl.textContent = `Start Timer`;
+      localStorage.setItem('time', 0);
 })
+
+window.addEventListener("DOMContentLoaded", () => {
+  totalTime = parseInt(localStorage.getItem("time"), 10) || 0;
+  timerEl.textContent =
+    totalTime > 0 ? `Time Left: ${totalTime}` : "Start Timer";
+  if (totalTime > 0) {
+    startTimer();
+  }
+});
