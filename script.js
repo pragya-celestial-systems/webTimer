f919fd82eaaebf77446aa2223deb021f5d7378a6
Celestial <celestial@CELSYSNCROOO036.local>
2024-10-04 11:32:38 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'usestrict';
+
 const inputContainer = document.querySelector("#topContainer");
 const minutesEl = document.querySelector("#minutes");
 const secondsEl = document.querySelector("#seconds");
@@ -15,13 +17,9 @@ let minutes = 0,
   totalTime = 0;
 let timer;
 
-localStorage.setItem("time", totalTime);
+// localStorage.setItem("time", totalTime);
 
 function startTimer() {
-  if (timer) {
-    clearInterval(timer);
-  }
-
   let time = localStorage.getItem("time");
   if (time > 0) {
     minutesEl.disabled = true;
@@ -34,15 +32,14 @@ function startTimer() {
       const remainingMinutes = Math.floor(time / 60) || 0;
       const remainingSeconds = time % 60 || 0;
       timerEl.textContent = `Time Left: ${remainingMinutes}:${remainingSeconds}`;
-      // startBtn.style.color = 'white';
-      // startBtn.style.backgroundColor = 'cadetblue';
-      // startBtn.style.opacity = 0.6;
       startBtn.classList.add('faded');
       startBtn.classList.remove('startBtn');
 
       if (time <= 0) {
         clearInterval(timer);
         timerEl.textContent = `Time up!!!`;
+        // for safety purpose so that everytime timer ends, the audio starts from begining
+        alarm.currentTime = 0;
         alarm.play();
         resetInputs();
       }
@@ -55,15 +52,10 @@ function resetInputs() {
   minutesEl.disabled = false;
   secondsEl.disabled = false;
   startBtn.disabled = false;
-  // disabl
-//   resetBtn.disabled = true;  
   minutesEl.value = "";
   secondsEl.value = "";
   startBtn.classList.add('startBtn');
   startBtn.classList.remove('faded');
-  // startBtn.style.color = "cadetblue";
-  // startBtn.style.backgroundColor = "white";
-  // startBtn.style.opacity = 1;
 }
 
 function setTotalTime() {
@@ -89,6 +81,11 @@ buttons.forEach((btn) => {
 
     setTotalTime();
     resetInputs();
+
+    if (timer) {
+      clearInterval(timer);
+    }
+
     startTimer();
   });
 });
@@ -114,7 +111,6 @@ secondsEl.addEventListener("input", (e) => {
     localStorage.setItem('time', 0);
     return;
   }
-
   
   seconds = parseInt(e.target.value) || 0;
   setTotalTime();
@@ -132,16 +128,17 @@ resetBtn.addEventListener('click', () => {
     if (timer) {
         clearInterval(timer);
     }
-      resetInputs()
-      timerEl.textContent = `Start Timer`;
-      localStorage.setItem('time', 0);
+  resetInputs()
+  alarm.pause();
+  timerEl.textContent = `Start Timer`;
+  localStorage.setItem('time', 0);
 })
 
 window.addEventListener("DOMContentLoaded", () => {
-  totalTime = parseInt(localStorage.getItem("time"), 10) || 0;
-  timerEl.textContent =
-    totalTime > 0 ? `Time Left: ${totalTime}` : "Start Timer";
-  if (totalTime > 0) {
+  const time = localStorage.getItem("time");
+  
+  if (time > 0) {
     startTimer();
   }
+
 });
